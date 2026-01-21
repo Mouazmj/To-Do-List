f0bcd9a6bc372a0fcb9cf978795b50da79182e63
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-21 12:24:35 +0100

    Add span style it to become an x for to be able to remove li

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const addTask = () => {
         let li = document.createElement('li')
         li.innerHTML = inputValue.value
         listContainer.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
     }
 
     inputValue.value = ''
