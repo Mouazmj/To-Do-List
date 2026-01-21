c2001f885ac7e5f99059ec6b89b486947a21618b
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-21 12:19:49 +0100

    Now being able to add text to the container with the button

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ let inputValue = document.getElementById('input-box')
 let listContainer = document.getElementById('list-container')
 
 const addTask = () => {
-    
+    if (inputValue.value === '') {
+        alert('Please type something')
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = inputValue.value
+        listContainer.appendChild(li)
+    }
+
+    inputValue.value = ''
 }
